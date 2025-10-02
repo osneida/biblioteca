f6082bfc231b5514be49f8f6b9468ed1eda5e6e3
osneida <osneidabordones@gmail.com>
2025-10-01 23:35:02 -0400

    iniciando la ruta para los enum

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     require __DIR__ . '/auth.php';
     require __DIR__ . '/admin.php';
+    require __DIR__ . '/enum.php';
 });
